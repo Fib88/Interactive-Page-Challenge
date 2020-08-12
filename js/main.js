6717645cc8a0693df0f33b51ce7914d261bcf50c
Huyen059 <samihuyen059@gmail.com>
2020-08-12 10:10:05 +0200

    add photo sliders

---
+++ js/main.js
@@ -0,0 +1,47 @@
+(function (){
+   /******PHOTO SLIDER******/
+   let imageSources = [
+       'pics/1.jpg',
+       'pics/2.jpg',
+       'pics/3.jpg',
+       'pics/4.jpeg',
+       'pics/5.jpeg'
+   ];
+
+    document.getElementById('next').addEventListener('click', ()=>{
+        let imagePosition = findImagePosition(imageSources);
+        let nextPosition;
+        if(imagePosition === imageSources.length - 1){
+            nextPosition = 0;
+        } else {
+            nextPosition = imagePosition + 1;
+        }
+        document.getElementById('picture').src = imageSources[nextPosition];
+    })
+
+    document.getElementById('previous').addEventListener('click', ()=>{
+        let imagePosition = findImagePosition(imageSources);
+        let prevPosition;
+        if(imagePosition === 0){
+            prevPosition = imageSources.length - 1;
+        } else {
+            prevPosition = imagePosition-1;
+        }
+
+        document.getElementById('picture').src = imageSources[prevPosition];
+    })
+
+})()
+
+function findImagePosition(imageSources)
+{
+    let pos = 0;
+
+    imageSources.forEach((src,index) => {
+        if(document.getElementById('picture').getAttribute('src') === src){
+            pos = index;
+        }
+    })
+
+    return pos;
+}
